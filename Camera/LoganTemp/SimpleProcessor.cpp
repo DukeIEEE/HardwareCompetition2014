e73fdd737492378e681f292b4132a0a206da23e2
lws <ludi1001@gmail.com>
2014-01-11 16:16:29 -0500

    filter by color, detect contours, average center of masses

--- Camera/LoganTemp/SimpleProcessor.cpp
+++ Camera/LoganTemp/SimpleProcessor.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <iostream>
+#define CALIBRATE_RED_MASK
 #include "SimpleProcessor.h"
 
 using namespace cv;
@@ -85,11 +87,11 @@ void SimpleProcessor::Process(cv::Mat frame) {
 	inRange(hsv, Scalar(0, slow, vlow), Scalar(hbegin, shigh, vhigh), red_mask1);
 	inRange(hsv, Scalar(hend,slow,vlow), Scalar(179,shigh,vhigh), red_mask2);
 #else
-	inRange(hsv, Scalar(0, 21, 21), Scalar(16, 235, 237), red_mask1);
-	inRange(hsv, Scalar(165, 21, 21), Scalar(179,235, 237), red_mask2);
+	inRange(hsv, Scalar(0, 146, 21), Scalar(17, 253, 255), red_mask1);
+	//inRange(hsv, Scalar(165, 21, 21), Scalar(179,235, 237), red_mask2);
 #endif
 
-	red_mask = red_mask1 | red_mask2;
+	red_mask = red_mask1;// | red_mask2;
 
 #ifdef CALIBRATE_WHITE_MASK
 	inRange(hsv, Scalar(hbegin, slow, vlow), Scalar(hend, shigh, vhigh), white_mask);
@@ -106,7 +108,8 @@ void SimpleProcessor::Process(cv::Mat frame) {
 	blur(white_mask, white_mask, Size(5,5));
 	threshold(white_mask, white_mask, 128, 255, CV_THRESH_BINARY);
 	//imshow("White Mask", white_mask);
-	//imshow("Red Mask", red_mask);
+	dilate(red_mask, red_mask, Mat());
+	imshow("Red Mask", red_mask);
 	//imshow("Mask", mask);
 	
 	Mat grayscale;
@@ -118,14 +121,15 @@ void SimpleProcessor::Process(cv::Mat frame) {
 	threshold(grayscale, out, 200, 255, CV_THRESH_BINARY);
 	erode(out, out, Mat());
 	dilate(out, out, Mat());
-	//namedWindow("Threshold");
-	//imshow("Threshold", out);
+	namedWindow("Threshold");
+	imshow("Threshold", out);
 
 	Mat combined = out & white_mask;
 	//namedWindow("Combined");
 	//imshow("Combined", combined);
 
-		//split image for sobel processing
+	
+	//split image for sobel processing
 	vector<Mat> planes;
 	split(equalized, planes);
 		Mat sobelR, sobelG, sobelB, sobelSat, sobelMask;
@@ -146,7 +150,58 @@ void SimpleProcessor::Process(cv::Mat frame) {
 	//imshow("SobelB", sobelB);
 	//imshow("SobelSat", sobelSat);
 
-	threshold(sobelMask, sobelMask, 180, 255, CV_THRESH_BINARY);
-	imshow("SobelMask", sobelMask);
+	//threshold(sobelMask, sobelMask, 180, 255, CV_THRESH_BINARY);
+	//imshow("SobelMask", sobelMask);
+	
+
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	vector<Point> contour_center;
+
+	RNG rng(12345);
+	/// Find contours
+  findContours( red_mask, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  /// Draw contours
+  //Mat drawing = Mat::zeros( sobelMask.size(), CV_8UC3 );
+  for( int i = 0; i< contours.size(); i++ )
+     {
+	   double contour_area = contourArea(contours[i]);
+	   if(contour_area < 1000) continue;
+	   Moments m = moments(contours[i]);
+	   Point center(m.m10/m.m00,m.m01/m.m00);
+	   contour_center.push_back(center);
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+	   circle( equalized, center, 3, Scalar(0,255,0), -1, 8, 0 );
+       drawContours( equalized, contours, i, color, 2, 8, hierarchy, 0, Point() );
+     }
+
+  if(contour_center.size() > 0) {
+	  //find contour center
+	  Point center(0,0);
+	  for(vector<Point>::const_iterator iter = contour_center.cbegin(); iter != contour_center.cend(); ++iter)
+		center += *iter;
+	  center.x /= contour_center.size();
+	  center.y /= contour_center.size();
+	  circle(equalized, center, 3, Scalar(255,0,0), -1, 8, 0);
+  }
+
+	/*vector<Vec4i> lines;
+	Mat sobel, sobelX, sobelY;
+Sobel(out,sobelX,CV_8U,1,0);
+Sobel(out,sobelY,CV_8U,0,1);
+sobel = sobelX + sobelY;
+HoughLinesP(sobel, lines, 1, CV_PI/180, 50, 50, 10 );
+  for( size_t i = 0; i < lines.size(); i++ )
+  {
+    Vec4i l = lines[i];
+    line( equalized, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+  }
+  namedWindow("sobel");
+  imshow("sobel", sobel);*/
+
+
+	//findContours(out, contours, 
 	set_img(equalized);
 }
