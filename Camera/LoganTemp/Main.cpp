3ea4a3c893fe78fdb27b738c621de6e15a55c8e3
mackyi <themackyi@gmail.com>
2014-02-08 14:29:31 -0500

    camera stuff
    more camera stuff

--- Camera/LoganTemp/Main.cpp
+++ Camera/LoganTemp/Main.cpp
@@ -7,19 +7,21 @@
 #include <memory>
 
 #include "SimpleProcessor.h"
-//#include "WhiteRectangleDetection.h"
-//#include "SurfProcessor.h"
+#include "WhiteRectangleDetection.h"
+#include "SurfProcessor.h"
 #include "RedWhiteProcessor.h"
-
 using namespace cv;
 
-int main0() {
-	std::unique_ptr<FrameProcessor> processor(new SimpleProcessor());//new WhiteRectangleDetection());
+int main() {
+	std::unique_ptr<FrameProcessor> processor(new SurfProcessor());//new WhiteRectangleDetection());
 	CvCapture* capture;
 	Mat frame;
 
 	//grab camera capture
 	capture = cvCaptureFromCAM(0);
+	if(!capture){
+		capture = cvCaptureFromCAM(-1);
+	}
 	if (capture) {
 		processor->PreProcess(capture);
 		while (true) {
@@ -36,30 +38,8 @@ int main0() {
 			if ((char) c == 'q') break;
 		}
 	}
-	return 0;
-}
-
-void main() {
-	std::unique_ptr<FrameProcessor> processor(new RedWhiteProcessor());//new WhiteRectangleDetection());
-	CvCapture* capture;
-	Mat frame;
-
-	//grab camera capture
-	capture = cvCaptureFromCAM(0);
-	if (capture) {
-		processor->PreProcess(capture);
-		while (true) {
-			frame = cvQueryFrame(capture);
-			if (!frame.empty())
-				processor->Process(frame);
-			else {
-				std::cerr << " --(!) No captured frame -- Break!" << std::endl;
-				break;
-			}
-
-			processor->Display();
-			int c = waitKey(10);
-			if ((char)c == 'q') break;
-		}
+	else {
+		return 1;
 	}
+	return 0;
 }
